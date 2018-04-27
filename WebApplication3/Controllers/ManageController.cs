62e2d03fe7df641562574d06f5a0ec631339bd6d
Quang Hoa <quanghoahcm@gmail.com>
2018-04-27 09:52:32 +0700

    qrcodejs

--- WebApplication3/Controllers/ManageController.cs
+++ WebApplication3/Controllers/ManageController.cs
@@ -518,11 +518,20 @@ namespace WebApplication3.Controllers
             return result.ToString().ToLowerInvariant();
         }
 
+        //private string GenerateQrCodeUri(string email, string unformattedKey)
+        //{
+        //    return string.Format(
+        //        AuthenticatorUriFormat,
+        //        _urlEncoder.Encode("Razor Pages"),
+        //        _urlEncoder.Encode(email),
+        //        unformattedKey);
+        //}
+
         private string GenerateQrCodeUri(string email, string unformattedKey)
         {
             return string.Format(
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("WebApplication3"),
+                _urlEncoder.Encode("Razor Pages"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
